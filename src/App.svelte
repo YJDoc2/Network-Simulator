75d9273f6fb5fca80b59a74b10dc35738550abd2
VatsalSoni_13 <sonivatsal2001@gmail.com>
2021-06-15 18:19:17 +0530

    Graph creation Modal (#2)
    * Graph creation modal
    
    * Graph creation modal
    
    * Graph creation modal

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,18 @@
 <script>
-  import NetworkEmulator from './content.svelte';
+  import NetworkEmulator from "./content.svelte";
+  import CreateGraph from "./modal.svelte";
   import {
     Header,
     HeaderUtilities,
     HeaderGlobalAction,
     SkipToContent,
-  } from 'carbon-components-svelte';
-  import CloudUpload32 from 'carbon-icons-svelte/lib/CloudUpload32';
-  import CloudDownload32 from 'carbon-icons-svelte/lib/CloudDownload32';
-  import Share32 from 'carbon-icons-svelte/lib/Share32';
-  import FetchUpload32 from 'carbon-icons-svelte/lib/FetchUpload32';
-  import Help32 from 'carbon-icons-svelte/lib/Help32';
-  import Save32 from 'carbon-icons-svelte/lib/Save32';
+  } from "carbon-components-svelte";
+  import CloudUpload32 from "carbon-icons-svelte/lib/CloudUpload32";
+  import CloudDownload32 from "carbon-icons-svelte/lib/CloudDownload32";
+  import Share32 from "carbon-icons-svelte/lib/Share32";
+  import FetchUpload32 from "carbon-icons-svelte/lib/FetchUpload32";
+  import Help32 from "carbon-icons-svelte/lib/Help32";
+  import Save32 from "carbon-icons-svelte/lib/Save32";
 </script>
 
 <Header company="" platformName="Network Emulator">
@@ -29,5 +30,6 @@
 </Header>
 
 <main style="margin-top:5rem">
+  <CreateGraph />
   <NetworkEmulator />
 </main>
---
+++ src/modal.svelte
@@ -0,0 +1,77 @@
+<script>
+  import { Modal } from "carbon-components-svelte";
+  let open = true;
+  let error = false;
+  let graphList = "";
+  let src = "utils/graph.png";
+  const check = () => {
+    if (graphList.length <= 0) {
+      error = true;
+    } else {
+      error = false;
+    }
+  };
+  // Function to pass the input to backend
+  const passGraphList = () => {
+    if (graphList.length <= 0) {
+      error = true;
+    } else {
+      open = false;
+      console.log(graphList.toLocaleUpperCase());
+    }
+  };
+</script>
+
+<Modal
+  bind:open
+  preventCloseOnClickOutside
+  size="lg"
+  modalHeading="Create Graph"
+  primaryButtonText="Create"
+  secondaryButtonText="Cancel"
+  on:click:button--secondary={() => {
+    open = false;
+  }}
+  on:open
+  on:close={() => {
+    open = false;
+  }}
+  on:submit={() => {
+    passGraphList();
+  }}
+>
+  <div class="modal-body">
+    <p>Example:</p>
+    <img {src} id="eg-img" alt="Graph" />
+    <p>List for above graph: "A-B A-C C-D"</p>
+  </div>
+  <div class="modal-body">
+    <label style="font-size: large; padding-top: 2em;"
+      >Enter input <input
+        type="text"
+        bind:value={graphList}
+        on:input={() => {
+          check();
+        }}
+      /></label
+    >
+
+    {#if error}
+      <p style="color: red;">Please enter valid input!</p>
+    {/if}
+  </div>
+</Modal>
+
+<style>
+  #eg-img {
+    height: 250px;
+    width: 300px;
+  }
+  input {
+    width: 20em;
+    border-radius: 15px;
+    border-color: black;
+    outline: none;
+    text-transform: uppercase;
+  }
+</style>
