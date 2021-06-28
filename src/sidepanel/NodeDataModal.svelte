f52182ed79cc3705ed7923ca603faa8fc65e52b9
VatsalSoni_13 <sonivatsal2001@gmail.com>
2021-06-28 22:41:10 +0530

    Editor in Config & added savetolocal in load project from lib (#16)
    * Graph creation modal
    
    * Graph creation modal
    
    * Download and Upload File
    
    * Download and Upload File
    
    * Drawing Component
    
    * Download & Upload File
    
    * Update
    
    * Retrieve data after refresh
    
    * Ctrl+S to Save
    
    * Ctrl+S to Save
    
    * Ctrl+S or Cmd+S to Save progress
    
    * Footer links connection
    
    * Documentation and sidebar fix
    
    * Examples and documentation
    
    * Graph creation modal
    
    * Graph creation modal
    
    * Download and Upload File
    
    * Download and Upload File
    
    * Drawing Component
    
    * Download & Upload File
    
    * Update
    
    * Retrieve data after refresh
    
    * Ctrl+S to Save
    
    * Ctrl+S to Save
    
    * Ctrl+S or Cmd+S to Save progress
    
    * Footer links connection
    
    * Documentation and sidebar fix
    
    * Update
    
    * Added Examples
    
    * Graph creation modal
    
    * Graph creation modal
    
    * Download and Upload File
    
    * Download and Upload File
    
    * Drawing Component
    
    * Download & Upload File
    
    * Update
    
    * Retrieve data after refresh
    
    * Ctrl+S to Save
    
    * Ctrl+S to Save
    
    * Ctrl+S or Cmd+S to Save progress
    
    * Footer links connection
    
    * Documentation and sidebar fix
    
    * Graph creation modal
    
    * Graph creation modal
    
    * Download and Upload File
    
    * Download and Upload File
    
    * Drawing Component
    
    * Download & Upload File
    
    * Update
    
    * Retrieve data after refresh
    
    * Ctrl+S to Save
    
    * Ctrl+S to Save
    
    * Ctrl+S or Cmd+S to Save progress
    
    * Footer links connection
    
    * Documentation and sidebar fix
    
    * Added Editor to Config with basic functions

--- src/sidepanel/NodeDataModal.svelte
+++ src/sidepanel/NodeDataModal.svelte
@@ -44,25 +44,63 @@
 
 <ComposedModal size="lg" bind:open on:open on:close on:submit={submit}>
   <ModalHeader label="Node {selected}" title="Node {selected} data" />
-  <ModalBody>
+  <ModalBody style="padding:10px">
     <Grid>
       <Row>
         <Column md={3}>
           <TextArea
             bind:value={mem}
+            id="memory"
             labelText="Node Memoey"
             placeholder="Node Memory"
             rows={25}
           />
+          <script>
+            var editor = CodeMirror.fromTextArea(
+              document.getElementById("memory"),
+              {
+                lineNumbers: true,
+                styleActiveLine: true,
+                matchBrackets: true,
+                mode: { json: true },
+                styleSelectedText: true,
+                indentWithTabs: true,
+                autoCloseBrackets: true,
+                lineWrapping: true,
+              }
+            );
+          </script>
         </Column>
-        <Column md={2} />
-        <Column md={3}>
+        <Column md={1} />
+        <Column md={4}>
           <TextArea
             bind:value={fn}
+            id="code"
             labelText="Node Function"
             placeholder="Node Function"
-            rows={25}
           />
+          <script>
+            var editor = CodeMirror.fromTextArea(
+              document.getElementById("code"),
+              {
+                lineNumbers: true,
+                styleActiveLine: true,
+                matchBrackets: true,
+                mode: "javascript",
+                styleSelectedText: true,
+                indentWithTabs: true,
+                autoCloseBrackets: true,
+                gutters: ["CodeMirror-lint-markers"],
+                lint: true,
+                lineWrapping: true,
+                // extraKeys: { "Ctrl-Space": "autocomplete" },
+              }
+            );
+            editor.setOption("extraKeys", {
+              "Cmd-E": "autocomplete",
+              "Ctrl-E": "autocomplete",
+            });
+          </script>
         </Column>
       </Row>
     </Grid>
