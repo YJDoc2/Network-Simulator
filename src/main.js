31a66aa186d534ad82c2f760936079c2374a9a88
VatsalSoni_13 <sonivatsal2001@gmail.com>
2021-06-30 12:56:34 +0530

    Update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import 'carbon-components-svelte/css/all.css';
-import App from './App.svelte';
+import "carbon-components-svelte/css/all.css";
+import App from "./App.svelte";
 
 const app = new App({
   target: document.body,
